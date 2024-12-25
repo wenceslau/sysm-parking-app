5bfd2789b04c86dfeb5ea5078c4b6d5832638174
Wenceslau Neto <wbaneto@gmail.com>
2024-12-25 12:33:15 +0000

    feat: add parking-application module
    Introduces the parking-application module to encapsulate application logic. It includes a `ParkingApp` class for managing parking operations and updates the parent `pom.xml` to include this new module. Dependencies and configuration for the module are defined in its dedicated `pom.xml`.

---
+++ parking-application/src/main/java/com/parking/application/ParkingApp.java
@@ -0,0 +1,50 @@
+package com.parking.application;
+
+import com.parking.*;
+
+import java.util.List;
+
+public class ParkingApp {
+
+    private final Parking parking;
+    private final ParkingGateway parkingGateway;
+
+    public ParkingApp(ParkingGateway parkingGateway) {
+        this.parkingGateway = parkingGateway;
+        var registrations = parkingGateway.loadAllByCurrentDay();
+        parking = new Parking(registrations);
+    }
+
+    public void openParking(int capacity) {
+        parking.openParking(capacity);
+    }
+
+    public Registration registerLicensePlate(String licensePlate, String vehicleType) {
+
+        // Convert the string to the VehicleType enum
+        var vehicleTypeEnum = VehicleType.converter(vehicleType);
+
+        double rate = parkingGateway.getRateByType(vehicleTypeEnum);
+
+        Vehicle vehicle = vehicleTypeEnum.getVehicle(licensePlate, rate);
+
+        var vehicleRegistration = this.parking.registerPlate(vehicle);
+
+        parkingGateway.save(vehicleRegistration);
+
+        return vehicleRegistration;
+    }
+
+    public List<Registration> vehiclesParked() {
+        return parking.getRegistrations().stream()
+                .filter(r -> r.getCheckOut() == null)
+                .toList();
+    }
+
+    public List<Registration> checkoutLog() {
+        return parking.getRegistrations().stream()
+                .filter(r -> r.getCheckOut() != null)
+                .toList();
+    }
+
+}
