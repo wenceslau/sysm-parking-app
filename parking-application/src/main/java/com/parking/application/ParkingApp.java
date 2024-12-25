8d8c9c0440dd3d9691a2a0522ab561ac9f500e15
Wenceslau Neto <wbaneto@gmail.com>
2024-12-25 16:49:41 +0000

    feat: create ParkingApp interface and implement it in ParkingAppImpl.
    Introduced a new ParkingApp interface defining core parking operations. Renamed and updated the existing ParkingApp class to ParkingAppImpl, ensuring it adheres to the new interface structure for better abstraction and future extensibility.

--- parking-application/src/main/java/com/parking/application/ParkingApp.java
+++ parking-application/src/main/java/com/parking/application/ParkingApp.java
@@ -1,50 +1,16 @@
 package com.parking.application;
 
-import com.parking.*;
+import com.parking.Registration;
 
 import java.util.List;
 
-public class ParkingApp {
+public interface ParkingApp {
 
-    private final Parking parking;
-    private final ParkingGateway parkingGateway;
+    void openParking(int capacity);
 
-    public ParkingApp(ParkingGateway parkingGateway) {
-        this.parkingGateway = parkingGateway;
-        var registrations = parkingGateway.loadAllByCurrentDay();
-        parking = new Parking(registrations);
-    }
+    Registration registerLicensePlate(String licensePlate, String vehicleType);
 
-    public void openParking(int capacity) {
-        parking.openParking(capacity);
-    }
+    List<Registration> vehiclesParked();
 
-    public Registration registerLicensePlate(String licensePlate, String vehicleType) {
-
-        // Convert the string to the VehicleType enum
-        var vehicleTypeEnum = VehicleType.converter(vehicleType);
-
-        double rate = parkingGateway.getRateByType(vehicleTypeEnum);
-
-        Vehicle vehicle = vehicleTypeEnum.getVehicle(licensePlate, rate);
-
-        var vehicleRegistration = this.parking.registerPlate(vehicle);
-
-        parkingGateway.save(vehicleRegistration);
-
-        return vehicleRegistration;
-    }
-
-    public List<Registration> vehiclesParked() {
-        return parking.getRegistrations().stream()
-                .filter(r -> r.getCheckOut() == null)
-                .toList();
-    }
-
-    public List<Registration> checkoutLog() {
-        return parking.getRegistrations().stream()
-                .filter(r -> r.getCheckOut() != null)
-                .toList();
-    }
-
+    List<Registration> checkoutLog();
 }
