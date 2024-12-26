2e147205626441398243b4bcfdfea4e67a8cfd8f
Wenceslau Neto <wbaneto@gmail.com>
2024-12-26 21:37:52 +0000

    reafctor: rename exception handler and add validation support
    Replaced `ExceptionHandler` with `AppExceptionHandler` to improve structure and extend functionality. Introduced handling for `MethodArgumentNotValidException` to return detailed validation error messages. Existing functionality for `RuntimeException` and generic exceptions remains intact.

---
+++ parking-infrastructure-spring/src/main/java/com/parking/infrastructure/configuration/AppExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.parking.infrastructure.configuration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.parking.infrastructure.configuration.AppExceptionHandler.ApiError.from;
+
+@RestControllerAdvice
+public class AppExceptionHandler {
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(from(ex));
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<?> handleException(Exception ex) {
+        return ResponseEntity
+                .internalServerError()
+                .body(from(ex));
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return errors;
+    }
+
+    record ApiError(String message) {
+        public static Object from(Exception ex) {
+            System.err.println(ex);
+            return new ApiError(ex.getMessage());
+        }
+    }
+
+}
